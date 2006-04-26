be73d4c3d3959eb6b46e2bbac9e134a949f66f32
Guilhem Vellut <guilhem.vellut+rubyforge@gmail.com>
2006-04-26 19:49:34 +0000

    Changed where needed references to the new GeoRuby API Added support for database to ruby schema and back : addition to the MySqlConnector of a options_for(table) method which returns a string indicating an engine. Modification of table and indexes method : table now makes use of option_for if present (right now only on MySqlConnector) to add options ; indexes can recognize spatial index and add the :spatial=> true option to the index. Correction of a bug on add_index where the name of the spatial index was not set correctly
    git-svn-id: http://georuby.rubyforge.org/svn/SpatialAdapter/trunk/mysql_spatial_adapter@18 7a115c08-5c0b-0410-86fa-965754e9f858

--- lib/mysql_spatial_adapter.rb
+++ lib/mysql_spatial_adapter.rb
@@ -56,17 +56,34 @@ module ActiveRecord
           nil
         end
       end
+
+      #used in migrations. 
+      def less_simplified_type(field_type)
+        case field_type
+        when /point/i then :point
+        when /linestring/i then :line_string
+        when /polygon/i then :polygon
+        when /geometry/i then :geometry
+        when /multipoint/i then :multi_point
+        when /multilinestring/i then :multi_line_string
+        when /multipolygon/i then :multi_polygon
+        when /geometrycollection/i then :geometry_collection
+        else original_simplified_type(field_type)
+        end
+      end
       
       private
       alias :original_simplified_type :simplified_type
       #Redefines the simplified_type method to add behabiour for when a column is of type geometry ; It should be enough to simplify all geometric types to :geometry. Unfortunately, if db:schema:dump is used (like for example during tests, when the database is regenerated), the dumped type for any of these geometric types will be :geometry. Maybe it is ok? It would simplify the code a little bit.
       def simplified_type(field_type)
         case field_type
-          when /point|linestring|polygon|geometry|multipoint|multilinestring|multipolygon|geometrycollection/i then :geometry
+        when /point|linestring|polygon|geometry|multipoint|multilinestring|multipolygon|geometrycollection/i then :geometry
         else original_simplified_type(field_type)
         end
       end
 
+      
+      
     end
 
     class MysqlAdapter
@@ -74,7 +91,7 @@ module ActiveRecord
       #Redefines the quote method to add behaviour for when a Geometry is encountered
       def quote(value, column = nil)
         if value.kind_of?(GeoRuby::SimpleFeatures::Geometry)
-          "GeomFromWKB(0x#{value.as_hex_binary(2,false)},#{value.srid})"
+          "GeomFromWKB(0x#{value.as_hex_wkb},#{value.srid})"
         else
           original_quote(value,column)
         end
@@ -99,7 +116,6 @@ module ActiveRecord
         if options[:spatial]
           #one by one
           Array(column_name).each do |col|
-            index_name = "#{table_name}_#{column_name}_index" if Array(column_name).length >0
             execute "CREATE SPATIAL INDEX #{index_name} ON #{table_name} (#{Array(column_name).join(", ")})"
           end
         else
@@ -108,9 +124,108 @@ module ActiveRecord
           execute "CREATE #{index_type} INDEX #{index_name} ON #{table_name} (#{Array(column_name).join(", ")})"
         end
       end
+
+      def indexes(table_name, name = nil)#:nodoc:
+        indexes = []
+        current_index = nil
+        execute("SHOW KEYS FROM #{table_name}", name).each do |row|
+          if current_index != row[2]
+            next if row[2] == "PRIMARY" # skip the primary key
+            current_index = row[2]
+            indexes << NewIndexDefinition.new(row[0], row[2], row[1] == "0", row[10]=="SPATIAL",[])
+          end
+          
+          indexes.last.columns << row[4]
+        end
+        indexes
+      end
+        
+      #to get the table creation options
+      def options_for(table)
+        result = execute("show table status like '#{table}'")
+        engine = result.fetch_row[1]
+        if engine !~ /inno/i #inno is default so do nothing for it in order not to clutter the migration
+          "ENGINE=#{engine}" 
+        else
+          nil
+        end
+      end
       
     end
   end
+
+  class NewIndexDefinition < Struct.new(:table, :name, :unique, :spatial,:columns)
+    
+  end
+
+  class SchemaDumper
+    def table(table, stream)
+      columns = @connection.columns(table)
+      begin
+        tbl = StringIO.new
+        
+        if @connection.respond_to?(:pk_and_sequence_for)
+          pk, pk_seq = @connection.pk_and_sequence_for(table)
+        end
+        pk ||= 'id'
+        
+        tbl.print "  create_table #{table.inspect}"
+        if columns.detect { |c| c.name == pk }
+          if pk != 'id'
+            tbl.print %Q(, :primary_key => "#{pk}")
+          end
+        else
+          tbl.print ", :id => false"
+        end
+        
+        if @connection.respond_to?(:options_for)
+          res = @connection.options_for(table)
+          tbl.print ", :options=>'#{res}'" if res
+        end
+              
+        tbl.print ", :force => true"
+        tbl.puts " do |t|"
+        
+        columns.each do |column|
+          raise StandardError, "Unknown type '#{column.sql_type}' for column '#{column.name}'" if @types[column.type].nil?
+          next if column.name == pk
+          tbl.print "    t.column #{column.name.inspect}, #{column.less_simplified_type(column.sql_type).inspect}"
+          tbl.print ", :limit => #{column.limit.inspect}" if column.limit != @types[column.type][:limit] 
+          tbl.print ", :default => #{column.default.inspect}" if !column.default.nil?
+          tbl.print ", :null => false" if !column.null
+          tbl.puts
+        end
+        
+        tbl.puts "  end"
+        tbl.puts
+        
+        indexes(table, tbl)
+        
+        tbl.rewind
+        stream.print tbl.read
+      rescue => e
+        stream.puts "# Could not dump table #{table.inspect} because of following #{e.class}"
+        stream.puts "#   #{e.message}"
+        stream.puts
+      end
+      
+      stream
+    end
+
+    def indexes(table, stream)
+      indexes = @connection.indexes(table)
+      indexes.each do |index|
+        stream.print "  add_index #{index.table.inspect}, #{index.columns.inspect}, :name => #{index.name.inspect}"
+        stream.print ", :unique => true" if index.unique
+        stream.print ", :spatial=> true " if index.spatial
+        stream.puts
+      end
+      
+      stream.puts unless indexes.empty?
+    end
+      
+  end
+
 end
 
 

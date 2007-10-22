d144cacb66dbae222b4b27330b3a8789f9d4b662
Guilhem Vellut <guilhem.vellut+rubyforge@gmail.com>
2007-10-22 11:22:10 +0000

    some changes (rob dupuis)
    git-svn-id: http://georuby.rubyforge.org/svn/SpatialAdapter/trunk/spatial_adapter@98 7a115c08-5c0b-0410-86fa-965754e9f858

--- init.rb
+++ init.rb
@@ -1,10 +1,14 @@
 class SpatialAdapterNotCompatibleError < StandardError
 end
 
-if ActiveRecord::Base.connection.is_a?(ActiveRecord::ConnectionAdapters::MysqlAdapter)
+adapter = ActiveRecord::Base.configurations[RAILS_ENV].symbolize_keys[:adapter]
+
+if adapter == "mysql"
   require 'mysql_spatial_adapter'
-elsif ActiveRecord::Base.connection.is_a?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+elsif adapter == "postgresql"
   require 'post_gis_adapter'
 else
   raise SpatialAdapterNotCompatibleError.new("Only MySQL and PostgreSQL are currently supported by the spatial adapter plugin.")
 end
+
+

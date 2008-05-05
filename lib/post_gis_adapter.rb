a5ca34a92903fb50d790601691d049ea0d6c9639
Pete Deffendol <pete@mdsc.com>
2008-05-05 14:47:31 -0600

    Fixed #disable_referential_integrity() after previous views update

--- lib/post_gis_adapter.rb
+++ lib/post_gis_adapter.rb
@@ -102,8 +102,12 @@ ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
 
   alias :original_tables :tables
   def tables(name = nil) #:nodoc:
+    original_tables(name) + views(name)
+  end
+  
+  def views(name = nil) #:nodoc:
     schemas = schema_search_path.split(/,/).map { |p| quote(p.strip) }.join(',')
-    original_tables(name) + query(<<-SQL, name).map { |row| row[0] }
+    query(<<-SQL, name).map { |row| row[0] }
       SELECT viewname
         FROM pg_views
         WHERE schemaname IN (#{schemas})
@@ -243,7 +247,7 @@ ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
     #Pete Deffendol's patch
     alias :original_disable_referential_integrity :disable_referential_integrity
     def disable_referential_integrity(&block) #:nodoc:
-      ignore_tables = %w{ geometry_columns spatial_ref_sys }
+      ignore_tables = %w{ geometry_columns spatial_ref_sys } + views
       execute(tables.select { |name| !ignore_tables.include?(name) }.collect { |name| "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER ALL" }.join(";"))
       yield
     ensure

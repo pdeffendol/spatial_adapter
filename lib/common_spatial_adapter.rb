a8383d1c42c30e97044ee5652f64b195cca9507c
rjft197 <robintapp@hotmail.com>
2009-08-21 21:07:54 -0600

    Fixes bug in creation of schema.rb for decimals and Dates/Times columns that have default values.

--- lib/common_spatial_adapter.rb
+++ lib/common_spatial_adapter.rb
@@ -55,7 +55,7 @@ ActiveRecord::SchemaDumper.class_eval do
         tbl.print ", :limit => #{column.limit.inspect}" if column.limit != @types[column.type][:limit] && column.precision.blank? && column.scale.blank?
         tbl.print ", :precision => #{column.precision.inspect}" if column.precision != @types[column.type][:precision]
         tbl.print ", :scale => #{column.scale.inspect}" if column.scale != @types[column.type][:scale] 
-        tbl.print ", :default => #{column.default.inspect}" if !column.default.nil?
+        tbl.print ", :default => #{default_string(column.default)}" if !column.default.nil?
         tbl.print ", :null => false" if !column.null
         tbl.puts
       end

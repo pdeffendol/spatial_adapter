82c3ca322935addb089d87dc2fe6ac4de6a95022
Pete Deffendol <pete@mdsc.com>
2010-02-16 11:36:09 -0700

    Stub :select_value in model insertion specs, because args to :insert vary
     * ActiveRecord 3.0.0 has a slightly different API, so this accounts for it

--- spec/postgis/models_spec.rb
+++ spec/postgis/models_spec.rb
@@ -11,67 +11,67 @@ describe ActiveRecord::ConnectionAdapters::PostgreSQLAdapter do
   describe "inserting records" do
     it 'should save Point objects' do
       model = PointModel.new(:extra => 'test', :geom => GeometryFactory.point)
-      @connection.should_receive(:insert).with(Regexp.new(GeometryFactory.point.as_hex_ewkb), anything(), anything(), anything(), anything())
+      @connection.should_receive(:select_value).with(Regexp.new(GeometryFactory.point.as_hex_ewkb))
       model.save.should == true
     end
   
     it 'should save LineString objects' do
       model = LineStringModel.new(:extra => 'test', :geom => GeometryFactory.line_string)
-      @connection.should_receive(:insert).with(Regexp.new(GeometryFactory.line_string.as_hex_ewkb), anything(), anything(), anything(), anything())
+      @connection.should_receive(:select_value).with(Regexp.new(GeometryFactory.line_string.as_hex_ewkb))
       model.save.should == true
     end
   
     it 'should save Polygon objects' do
       model = PolygonModel.new(:extra => 'test', :geom => GeometryFactory.polygon)
-      @connection.should_receive(:insert).with(Regexp.new(GeometryFactory.polygon.as_hex_ewkb), anything(), anything(), anything(), anything())
+      @connection.should_receive(:select_value).with(Regexp.new(GeometryFactory.polygon.as_hex_ewkb))
       model.save.should == true
     end
   
     it 'should save MultiPoint objects' do
       model = MultiPointModel.new(:extra => 'test', :geom => GeometryFactory.multi_point)
-      @connection.should_receive(:insert).with(Regexp.new(GeometryFactory.multi_point.as_hex_ewkb), anything(), anything(), anything(), anything())
+      @connection.should_receive(:select_value).with(Regexp.new(GeometryFactory.multi_point.as_hex_ewkb))
       model.save.should == true
     end
   
     it 'should save MultiLineString objects' do
       model = MultiLineStringModel.new(:extra => 'test', :geom => GeometryFactory.multi_line_string)
-      @connection.should_receive(:insert).with(Regexp.new(GeometryFactory.multi_line_string.as_hex_ewkb), anything(), anything(), anything(), anything())
+      @connection.should_receive(:select_value).with(Regexp.new(GeometryFactory.multi_line_string.as_hex_ewkb))
       model.save.should == true
     end
   
     it 'should save MultiPolygon objects' do
       model = MultiPolygonModel.new(:extra => 'test', :geom => GeometryFactory.multi_polygon)
-      @connection.should_receive(:insert).with(Regexp.new(GeometryFactory.multi_polygon.as_hex_ewkb), anything(), anything(), anything(), anything())
+      @connection.should_receive(:select_value).with(Regexp.new(GeometryFactory.multi_polygon.as_hex_ewkb))
       model.save.should == true
     end
   
     it 'should save GeometryCollection objects' do
       model = GeometryCollectionModel.new(:extra => 'test', :geom => GeometryFactory.geometry_collection)
-      @connection.should_receive(:insert).with(Regexp.new(GeometryFactory.geometry_collection.as_hex_ewkb), anything(), anything(), anything(), anything())
+      @connection.should_receive(:select_value).with(Regexp.new(GeometryFactory.geometry_collection.as_hex_ewkb))
       model.save.should == true
     end
   
     it 'should save Geometry objects' do
       model = GeometryModel.new(:extra => 'test', :geom => GeometryFactory.point)
-      @connection.should_receive(:insert).with(Regexp.new(GeometryFactory.point.as_hex_ewkb), anything(), anything(), anything(), anything())
+      @connection.should_receive(:select_value).with(Regexp.new(GeometryFactory.point.as_hex_ewkb))
       model.save.should == true
     end
   
     it 'should save 3D Point (with Z coord) objects' do
       model = PointzModel.new(:extra => 'test', :geom => GeometryFactory.pointz)
-      @connection.should_receive(:insert).with(Regexp.new(GeometryFactory.pointz.as_hex_ewkb), anything(), anything(), anything(), anything())
+      @connection.should_receive(:select_value).with(Regexp.new(GeometryFactory.pointz.as_hex_ewkb))
       model.save.should == true
     end
   
     it 'should save 3D Point (with M coord) objects' do
       model = PointmModel.new(:extra => 'test', :geom => GeometryFactory.pointm)
-      @connection.should_receive(:insert).with(Regexp.new(GeometryFactory.pointm.as_hex_ewkb), anything(), anything(), anything(), anything())
+      @connection.should_receive(:select_value).with(Regexp.new(GeometryFactory.pointm.as_hex_ewkb))
       model.save.should == true
     end
   
     it 'should save 4D Point objects' do
       model = Point4Model.new(:extra => 'test', :geom => GeometryFactory.point4)
-      @connection.should_receive(:insert).with(Regexp.new(GeometryFactory.point4.as_hex_ewkb), anything(), anything(), anything(), anything())
+      @connection.should_receive(:select_value).with(Regexp.new(GeometryFactory.point4.as_hex_ewkb))
       model.save.should == true
     end
   end
